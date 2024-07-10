bdd58324070f5759e04bea39403c5c70c498234d
andrechagaslima <andrechalima@gmail.com>
2024-07-09 22:24:20 -0300

    Adicionando a condição para números negativos

--- modulos/dp.h
+++ modulos/dp.h
@@ -1,9 +1,15 @@
 #ifndef DP_H
 #define DP_H
+#include <stdbool.h>
+
+typedef struct ArrayAuxiliar {
+    unsigned long int valor;
+    bool calculado;
+} ArrayAuxiliar;
 
 int maxDP(int a, int b);
 
-unsigned long int solutionDP(unsigned long int *vet, int n, int i, unsigned long int *dp);
+unsigned long int solutionDP(unsigned long int *vet, int n, int i, ArrayAuxiliar *dp);
 
 unsigned long int dynamic(unsigned long int *vet, int n);
 
--- principal/dp.c
+++ principal/dp.c
@@ -10,27 +10,27 @@ int maxDP(int a, int b){
     }
 }
 
-unsigned long int solutionDP(unsigned long int *vet, int n, int i, unsigned long int *dp){
+unsigned long int solutionDP(unsigned long int *vet, int n, int i, ArrayAuxiliar *dp){
     if (i >= n) {
         return 0;
     }
 
-    if (dp[i] != -1) {
-        return dp[i];
+    if (dp[i].calculado != false) {
+        return dp[i].valor;
     }
 
     unsigned long int valor1 = solutionDP(vet, n, i + 1, dp);
     unsigned long int valor2 = vet[i] + solutionDP(vet, n, i + 2, dp);
 
-    dp[i] = maxDP(valor1, valor2);
-
-    return dp[i];
+     dp[i].valor = maxDP(valor1, valor2);
+     dp[i].calculado = true;
+    return dp[i].valor;
 }
 
 unsigned long int dynamic(unsigned long int *vet, int n){
-    unsigned long int *dp = (unsigned long int *)malloc((n) * sizeof(unsigned long int));
+    ArrayAuxiliar *dp = (ArrayAuxiliar *)malloc((n) * sizeof(ArrayAuxiliar));
     for (int i = 0; i < n; i++) {
-        dp[i] = -1;
+        dp[i].calculado = false;
     }
 
     unsigned long int result = solutionDP(vet, n, 0, dp);
